b9047dddd0ee5be9ea81e459a601bc0eec2d4f02
Toby Berster <wurzelgogerer@gmail.com>
2012-10-19 16:29:52 -0700

    proper parameters

--- APNSWrapper/connection.py
+++ APNSWrapper/connection.py
@@ -157,7 +157,7 @@ class M2CryptoModuleConnection(APNSConnectionContext):
         self.socket = socket.socket()
         ctx = self.ssl_module.Context('sslv3')
         if self.passphrase:
-            ctx.load_cert(certchainfile=self.certificate,
+            ctx.load_cert(certfile=self.certificate,
                             callback=self.get_passphrase)
         else:
             ctx.load_cert(certfile=self.certificate)
