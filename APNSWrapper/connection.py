874d4df5b0cede504bf7e936ac68373b3cf91b6b
Toby Berster <wurzelgogerer@gmail.com>
2012-10-19 17:09:15 -0700

    another attempt to load passphrase

--- APNSWrapper/connection.py
+++ APNSWrapper/connection.py
@@ -159,7 +159,7 @@ class M2CryptoModuleConnection(APNSConnectionContext):
             #ctx.load_cert(self.certificate,
             #                callback=lambda *args:open(self.passphrase, 'r').readline().strip())
             ctx.load_cert(self.certificate,
-                            callback=lambda:"onpatient")
+                            callback=self.get_passphrase)
         else:
             ctx.load_cert(self.certificate)
 
@@ -170,6 +170,10 @@ class M2CryptoModuleConnection(APNSConnectionContext):
     def certificate(self, path):
         self.certificate = path
         return self
+
+    def get_passphrase(self):
+        print "passphrase"
+        return "passphrase"
 
     def passphrase(self, path):
         self.passphrase = path
